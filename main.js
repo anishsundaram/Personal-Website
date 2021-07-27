c4936874c27c11fc1a742b8d4274405ecc5bf4a5
Anish Sundaram <anishsun@umich.edu>
2021-07-26 23:57:57 -0700

    Add Typewriter effect and lower title gap

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Typewriter = function(txtElement, words, wait=3000){
+const TypeWriter = function(txtElement, words, wait=3000){
     this.txtElement = txtElement;
     this.words = words;
     this.txt ='';
@@ -8,6 +8,27 @@ const Typewriter = function(txtElement, words, wait=3000){
     this.isDeleting = false;
 }
 
+TypeWriter.prototype.type= function(){
+    const current = this.wordIndex % this.words.length;
+
+    const fullTxt = this.words[current];
+    
+    if(this.isDeleting){
+        this.txt= fullTxt.substring(0, this.txt.length - 1);
+
+    }
+
+    else{
+        this.txt= fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+
+    setTimeout(() => this.type(), 500)
+}
+
+
 document.addEventListener('DOMContentLoaded',init);
 
 function init(){
@@ -15,5 +36,5 @@ function init(){
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
 
-    new Typewriter(txtElement,words,wait);
+    new TypeWriter(txtElement,words,wait);
 }
