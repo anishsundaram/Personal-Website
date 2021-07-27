a44ecd15af6d187ce54de66319a13279f02ac3cc
Anish Sundaram <anishsun@umich.edu>
2021-07-27 00:14:15 -0700

    Increase speed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const TypeWriter = function(txtElement, words, wait=3000){
+const TypeWriter = function(txtElement, words, wait=2000){
     this.txtElement = txtElement;
     this.words = words;
     this.txt ='';
@@ -23,9 +23,9 @@ TypeWriter.prototype.type= function(){
 
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    let typeSpeed = 300;
+    let typeSpeed = 200;
     if(this.isDeleting){
-        typeSpeed /= 2;
+        typeSpeed /= 3;
     }
 
     if(!this.isDeleting && fullTxt === this.txt){
@@ -37,7 +37,7 @@ TypeWriter.prototype.type= function(){
         this.isDeleting = false;
         this.wordIndex++;
 
-        typeSpeed = 500;
+        typeSpeed = 400;
     }
     
     setTimeout(() => this.type(), typeSpeed)
